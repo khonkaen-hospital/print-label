4245799fe3bdf00b36459b5d6eedafc6f0a2e8c6
Sathit Seethaphon <dixonsatit@gmail.com>
2021-01-09 21:26:06 +0700

    init deployment

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -t docker.pkg.github.com/khonkaen-hospital/print-label/print-label .
-docker push docker.pkg.github.com/khonkaen-hospital/print-label/print-label
--- src/routes/Footnote.ts
+++
@@ -1,8 +0,0 @@
-import * as express from 'express';
-import { FootnoteController } from '../controllers/FootnoteController';
-const router = express.Router();
-
-router.get('/ipd/:an/:copies', FootnoteController.ipd);
-router.get('/opd/:vn/:copies', FootnoteController.opd);
-
-export default router;
