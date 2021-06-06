5a1e0035f8bc367567c3b96ece02c3bdc5000d41
Sathit Seethaphon <dixonsatit@gmail.com>
2021-06-06 23:40:20 +0700

    add copy suppor print

--- src/controllers/VaccineController.ts
+++ src/controllers/VaccineController.ts
@@ -13,16 +13,18 @@ export type CreateItem = (doc: jsPDF, data: Array<any>, addPage: boolean) => any
 const model = new HisModel();
 
 export class VaccineController {
+
 	public static async ipd(req: Request, res: Response, next: NextFunction) {
 		const db = req.db;
 		let data: Array<any> = [];
 		let sn = [];
 		sn = req.params.sn.split(',');
+		const copy = req.query.copy ? +req.query.copy : 1;
 
 		if (db !== undefined) {
 			data = await model.getVaccineBySn(db, sn);
 		}
-		const [content, tempName] = createPdf(data, createItemTemplateIpd);
+		const [content, tempName] = createPdf(data, copy, createItemTemplate);
 
 		res.filename = tempName;
 		res.set({ 'content-type': 'application/pdf; charset=utf-8' });
@@ -32,7 +34,7 @@ export class VaccineController {
 }
 
 
-function createPdf(data: Array<any>, createItem: CreateItem) {
+function createPdf(data: Array<any>, copy: number, createItem: CreateItem) {
 	const tempName: string = './temp/' + nanoid() + '.pdf';
 	const pdf = new PDF({
 		orientation: "landscape",
@@ -40,9 +42,25 @@ function createPdf(data: Array<any>, createItem: CreateItem) {
 		format: [76.2, 50.8]
 	});
 	if (data.length > 0) {
-		data.forEach((value, index) => {
-			createItem(pdf.doc, value, (index === (data.length - 1) ? false : true));
+		const printItems: Array<any> = [];
+		data.forEach((v, i) => {
+			let item = JSON.parse(JSON.stringify(v));
+			const maxDose = +v.max_tolerated_dose || 1;
+			if (maxDose > 1) {
+				for (let index = 0; index < maxDose; index++) {
+					let newIitem = JSON.parse(JSON.stringify(item));
+					newIitem.dose_no = index + 1;
+					printItems.push(newIitem);
+				}
+			} else {
+				printItems.push(item);
+			}
+		});
 
+		printItems.forEach((value, index) => {
+			for (let copyIndex = 0; copyIndex < copy; copyIndex++) {
+				createItem(pdf.doc, value, (index === (printItems.length - 1) && copyIndex === (copy - 1) ? false : true));
+			}
 		});
 	}
 	pdf.doc.save(tempName);
@@ -50,7 +68,7 @@ function createPdf(data: Array<any>, createItem: CreateItem) {
 	return [pdfContent, tempName];
 }
 
-async function createItemTemplateIpd(doc: jsPDF, data: any, addPage: boolean) {
+function createItemTemplate(doc: jsPDF, data: any, addPage: boolean) {
 
 	const canvas = createCanvas(100, 100);
 	JsBarcode(canvas, data.dose_id, {
@@ -63,19 +81,28 @@ async function createItemTemplateIpd(doc: jsPDF, data: any, addPage: boolean) {
 	doc.setFont("SarabunNew", 'normal');
 	doc.setFontSize(18);
 	doc.text("LN:", 5, 6);
-	doc.text(data.lot_number.toUpperCase(), 16, 6);
+	doc.setFont("SarabunNewBold", 'bold');
+	doc.text(data.lot_number.toUpperCase(), 15, 6);
 
 	doc.setFont("SarabunNew", 'normal');
 	doc.text("SN:", 5, 14);
-	doc.text(data.vaccine_serial_no, 16, 14);
+	doc.setFont("SarabunNewBold", 'bold');
+	doc.text(data.vaccine_serial_no, 15, 14);
 
 	doc.setFont("SarabunNew", 'normal');
 	doc.text("Exp:", 5, 22);
-	doc.text(moment(data.expiration_date).format('YYYY-MM'), 18, 22);
+	doc.setFont("SarabunNewBold", 'bold');
+	doc.text(moment(data.expiration_date).format('YYYY-MM'), 15, 22);
 
 	doc.setFont("SarabunNew", 'normal');
-	doc.text("ขวดที่:", 38, 22);
-	doc.text(data.bottle_no || '-', 50, 22);
+	doc.text("ขวดที่:", 33, 22);
+	doc.setFont("SarabunNewBold", 'bold');
+	doc.text(data.bottle_no || '-', 45, 22);
+
+	doc.setFont("SarabunNew", 'normal');
+	doc.text("โดสที่:", 53, 22);
+	doc.setFont("SarabunNewBold", 'bold');
+	doc.text(+data.max_tolerated_dose > 1 ? data.dose_no.toString() : '1', 64, 22);
 
 	doc.setFont("SarabunNewBold", 'bold');
 	const pageWidth = doc.internal.pageSize.getWidth();
@@ -83,7 +110,6 @@ async function createItemTemplateIpd(doc: jsPDF, data: any, addPage: boolean) {
 	if (addPage === true) {
 		doc.addPage();
 	}
-
 }
 
 
