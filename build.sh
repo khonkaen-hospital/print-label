d43700256c225321a084f8978dee3268de06466c
Sathit Seethaphon <dixonsatit@gmail.com>
2022-01-14 22:04:39 +0700

    update version 1.0.1

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t docker.pkg.github.com/khonkaen-hospital/print-label/print-label .
-docker push docker.pkg.github.com/khonkaen-hospital/print-label/print-label
+docker build -t docker.pkg.github.com/khonkaen-hospital/print-label/print-label:1.0.1 .
+docker push docker.pkg.github.com/khonkaen-hospital/print-label/print-label:1.0.1
