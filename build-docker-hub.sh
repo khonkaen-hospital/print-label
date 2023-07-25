285556ef5c4f65cec95cb7247acfdd4626070222
Sathit Seethaphon <dixonsatit@gmail.com>
2023-07-26 01:34:28 +0700

    add buildx

--- build-docker-hub.sh
+++ build-docker-hub.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t dixonsatit/print-label:1.0.2 .
+docker buildx build  --platform linux/amd64  -t dixonsatit/print-label:1.0.2 .
 docker push dixonsatit/print-label:1.0.2
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t ghcr.io/khonkaen-hospital/print-label/print-label:1.0.2 .
+docker buildx build  --platform linux/amd64 -t ghcr.io/khonkaen-hospital/print-label/print-label:1.0.2 .
 docker push ghcr.io/khonkaen-hospital/print-label/print-label:1.0.2
